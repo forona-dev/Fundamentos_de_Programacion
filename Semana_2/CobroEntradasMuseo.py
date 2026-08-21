e4dfabd98fdda07d2f11154883b86f759a7614c3
Filiberto Orona Loera <filiorona97.loera@gmail.com>
2026-08-21 11:26:05 -0600

    Subo carpeta de semana 2

---
+++ Semana_2/CobroEntradasMuseo.py
@@ -0,0 +1,6 @@
+# Variables
+acumulador = 0      # Aqui voy a ir sumando el total de las entradas 
+edad = int(input())
+
+print("Bienvenido al Museo de Antropologia e Historia, ¿Cúantas personas asistirán?")
+visitantes = int(input())
